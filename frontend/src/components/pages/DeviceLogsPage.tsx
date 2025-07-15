bf2b5088368815200e019668b53a83b5ef242c1a
Benjamin <benjamin.kaliope@gmail.com>
2025-07-15 16:34:59 -0400

    Bug fixes and linking device logs page

---
+++ frontend/src/components/pages/DeviceLogsPage.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import DeviceModal from '../DeviceModal';
+export default function DeviceLogsPage()
+{
+    return(
+        <h1>Sigma</h1>
+    );
+}
