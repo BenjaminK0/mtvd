b5adbbde756bcd3c0c8443689d4c010826917598
Benjamin <benjamin.kaliope@gmail.com>
2025-07-15 12:54:07 -0400

    Outlook Calendar Added

--- frontend/src/components/pages/CalendarPage.tsx
+++ frontend/src/components/pages/CalendarPage.tsx
@@ -2,5 +2,14 @@
 
 export default function CalendarPage()
 {
-    
-}
+    return(
+        <iframe
+      src="https://outlook.live.com/owa/calendar/00000000-0000-0000-0000-000000000000/00f8b3e3-1571-4c24-a380-5f35d7cc23c6/cid-453906B01755C9C7/index.html"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        border: 'none'
+      }}
+    />
+    )
+   }
